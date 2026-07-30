ef9471b3f2b8b9cc7f05c66974490c270f3f7822
André Gustavo <4ndr3g987.not@gmail.com>
2026-07-30 18:34:09 -0300

    Adiciona novos componentes CardTarefa e Footer, e atualiza o layout principal

---
+++ commit-focus/src/components/CardTarefa/CardTarefa.tsx
@@ -0,0 +1,9 @@
+// import style from './CardTarefa.module.css'
+
+export function CardTarefa() {
+  return (
+    <div>
+      <h1>Card Tarefa</h1>
+    </div>
+  )
+}
--- commit-focus/src/layout/MainLayout/MainLayout.tsx
+++ commit-focus/src/layout/MainLayout/MainLayout.tsx
@@ -1,6 +1,8 @@
 import style from './MainLayout.module.css';
 import { Header } from "../../components/Header/Header";
 import { InputAddTarefa } from '../../components/InputAddTarefa/InputAddTarefa';
+import { Footer } from '../../components/Footer/Footer';
+import { CardTarefa } from '../../components/CardTarefa/CardTarefa';
 
 export function MainLayout() {
   return (
@@ -10,6 +12,9 @@ export function MainLayout() {
           <Header />
           <InputAddTarefa />
           <hr />
+          <CardTarefa />
+          <hr />
+          <Footer />
         </div>  
       </div> 
     </>
